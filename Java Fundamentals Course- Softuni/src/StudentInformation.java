e25102705dae737e737b01aa2b3d11660dbf2163
Emil Voykov <75309274+emilVoykovTh@users.noreply.github.com>
2020-12-05 00:31:53 +0200

    Add files via upload

---
+++ Java Fundamentals Course- Softuni/src/StudentInformation.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class StudentInformation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String studentName = scanner.nextLine();
+        int studentAge = Integer.parseInt(scanner.nextLine());
+        double averageGrade = Double.parseDouble(scanner.nextLine());
+        System.out.printf("Name: %s, Age: %d, Grade: %.2f",studentName, studentAge, averageGrade);
+    }
+}
