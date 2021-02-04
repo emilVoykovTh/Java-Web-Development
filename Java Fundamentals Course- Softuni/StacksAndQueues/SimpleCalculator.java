7d5440228bf2a2c3e3042516a0e26ae5fc0d57c3
Emil Voykov <75309274+emilVoykovTh@users.noreply.github.com>
2021-02-04 19:36:44 +0200

    Some of my resolved Exercises from Stacks & Queues

---
+++ Java Fundamentals Course- Softuni/StacksAndQueues/SimpleCalculator.java
@@ -0,0 +1,10 @@
+package StacksAndQueues;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
