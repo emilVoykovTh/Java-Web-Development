1c702ffd280a605b80ae32967a273d5b2b1fa797
Emil Voykov <75309274+emilVoykovTh@users.noreply.github.com>
2021-02-18 18:08:51 +0200

    small change in CustomMinFunction

--- Java Advanced Course/src/FunctionalProgramming/CustomMinFunction.java
+++ Java Advanced Course/src/FunctionalProgramming/CustomMinFunction.java
@@ -7,7 +7,6 @@ import java.util.regex.Matcher;
 public class CustomMinFunction {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int min = Integer.MAX_VALUE;
        int num = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).min().getAsInt();
         System.out.println(num);
     }
