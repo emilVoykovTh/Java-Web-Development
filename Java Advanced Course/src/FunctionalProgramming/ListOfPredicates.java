d3cc462df09269a12d9c384063c0db50c3ddca18
Emil Voykov <75309274+emilVoykovTh@users.noreply.github.com>
2021-02-18 22:52:21 +0200

    Some more FunctionalProgramming resolutions

---
+++ Java Advanced Course/src/FunctionalProgramming/ListOfPredicates.java
@@ -0,0 +1,33 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ListOfPredicates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Set<Integer> nums = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).boxed()
+                .collect(Collectors.toSet());
+        Predicate<Integer> isDivisible = num -> {
+            for (Integer divider : nums) {
+                if (num % divider != 0) {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        for (int i = 1; i <= n; i++) {
+            boolean res = isDivisible.test(i);
+            if (res) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
